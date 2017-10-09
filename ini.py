dde0ae202193229fd30970f3cec2806e3d8787df
Diego <dieguitobugs@hotmail.com>
2017-10-09 19:23:04 -0300

    Detección de coordenadas dentro de un polygono establecido

--- ini.py
+++ ini.py
@@ -7,7 +7,7 @@ from matplotlib import path
 #POLIGONO = path.Path([(-25.273395,-57.649131),(-25.291854,-57.670566),(-25.379863,-57.623205),(-25.282071,-57.540120),(-25.254011,-57.584827),(-25.273395,-57.649131)])
 
 #POLIGONO DE ENCARNACION
-POLIGONO = path.Path([(-27.275677,-56.648758),(-26.932586,-56.100027),(-27.287288,-55.678680),(-27.548231,-55.959578),(-27.275677,-56.648758)])
+POLIGONO = path.Path([(-27.930452,-57.444440),(-25.596055,-56.774274),(-25.962093,-54.357282),(-29.204053,-55.324079),(-27.930452,-57.444440)])
 
 
 def inside_polygon(x, y, POLIGONO):
@@ -32,8 +32,9 @@ _columns = ["type",
                "ullongitude",
                "lrlatitude",
                "lrlongitude"]
-raios=0
-rowsAnalized = 0
+rowsAnalized=0
+raios= 0
+timesThousands = 1
 for row in pd.read_csv(inFile,sep=',', chunksize=1):
     latitude = row.latitude.item()
     longitude = row.longitude.item()
@@ -44,9 +45,9 @@ for row in pd.read_csv(inFile,sep=',', chunksize=1):
         print(latitude)
         print(longitude)
         break
-    if(rowsAnalized>10000):
+    if(rowsAnalized==(10000*timesThousands)):
         print(rowsAnalized)
-        rowsAnalized=0
+        timesThousands+=1
 
-
+print(rowsAnalized)
 print("Rayos adentro:",raios,sep=" ")
