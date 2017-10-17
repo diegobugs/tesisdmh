5bca017423a25b5d6e339178ed31a10f16715f13
Diego <dieguitobugs@hotmail.com>
2017-10-17 20:55:08 -0300

    Documentación de desarrollo

--- ini.py
+++ ini.py
@@ -5,7 +5,7 @@ from util import DatabaseConnection as db
 
 if __name__ == '__main__':
     database_connection = db.DatabaseConnection()
-    rows = database_connection.query("SELECT * FROM lightning_data LIMIT 70000")
+    rows = database_connection.query("SELECT * FROM lightning_data LIMIT 100")
     for row in rows:
         # print("data: {0}".format(row))
         start_time = row[3]
@@ -20,6 +20,15 @@ if __name__ == '__main__':
         cg_multiplicity = row[12]
         geom = row[13]
 
-        print(latitude)
+        # print(latitude)
+
+        """"
+        1. Recorrer por tipo de descarga
+        2. Recorrer mientras tiempo incio sea menor a tiempo final
+        3. Sumar tiempo en lapso de 10 minutos
+        4. recoger descargas dentro de coordenadas dadas, tipo de rayo y tiempo inicio y fin
+        5. Sumar valor absoluto de peak_current
+        6. detectar peak_current mayor a 1.000.000 de Amperios        
+        """
 
 polygon = Polygon([(-27.930452,-57.444440),(-25.596055,-56.774274),(-25.962093,-54.357282),(-29.204053,-55.324079),(-27.930452,-57.444440)])
