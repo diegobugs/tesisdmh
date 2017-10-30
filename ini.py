a4e71c15323319ddf0783431777aa378a1ecd9f2
Diego <dieguitobugs@hotmail.com>
2017-10-29 21:24:20 -0300

    Graficar descargas en un mapa

--- ini.py
+++ ini.py
@@ -2,13 +2,20 @@ import pandas as pd
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 from util import DatabaseConnection as db
+from util import PlotData as plot
 from datetime import datetime
 from datetime import timedelta
 
 if __name__ == '__main__':
+
+    plot = plot.Plot()
+    # plot.drawIntoMap(-57.623154, -25.280073,1)
+    # plot.drawIntoMap(-57.603154, -25.284073, 1)
+    # plot.printMap()
+    # exit(0)
     database_connection = db.DatabaseConnection()
-    diaAnalizarIni = datetime.strptime('2016-11-01 00:00:00', '%Y-%m-%d %H:%M:%S')
-    diaAnalizarFin = datetime.strptime('2016-11-30 23:59:59', '%Y-%m-%d %H:%M:%S')
+    diaAnalizarIni = datetime.strptime('2016-11-27 00:00:00', '%Y-%m-%d %H:%M:%S')
+    diaAnalizarFin = datetime.strptime('2016-11-27 23:59:59', '%Y-%m-%d %H:%M:%S')
 
     coordenadaAnalizar = '-57.606765,-25.284659'
     diametroAnalizar = '30000' #en metros
@@ -19,7 +26,7 @@ if __name__ == '__main__':
     tiempoAnalizarFin = tiempoAnalizarIni + timedelta(minutes=tiempoIntervalo)
 
     rows = database_connection.query(
-        "SELECT start_time,end_time,type,latitude,longitude,peak_current,ic_height,number_of_sensors,ic_multiplicity,cg_multiplicity,geom FROM lightning_data WHERE type=1 AND ST_DistanceSphere(geom, ST_MakePoint(" + coordenadaAnalizar + ")) <= " + diametroAnalizar + "  AND start_time >= to_timestamp('" + str(diaAnalizarIni) + "', 'YYYY-MM-DD HH24:MI:SS.MS') AND start_time <= to_timestamp('" + str(diaAnalizarFin) + "', 'YYYY-MM-DD HH24:MI:SS.MS')")
+        "SELECT start_time,end_time,type,latitude,longitude,peak_current,ic_height,number_of_sensors,ic_multiplicity,cg_multiplicity,geom FROM lightning_data WHERE type=0 AND ST_DistanceSphere(geom, ST_MakePoint(" + coordenadaAnalizar + ")) <= " + diametroAnalizar + "  AND start_time >= to_timestamp('" + str(diaAnalizarIni) + "', 'YYYY-MM-DD HH24:MI:SS.MS') AND start_time <= to_timestamp('" + str(diaAnalizarFin) + "', 'YYYY-MM-DD HH24:MI:SS.MS')")
     df = pd.DataFrame(data=rows, columns=['start_time','end_time','type','latitude','longitude','peak_current','ic_height','number_of_sensors','ic_multiplicity','cg_multiplicity','geom'])
 
     while tiempoAnalizarIni <= diaAnalizarFin:
@@ -36,11 +43,13 @@ if __name__ == '__main__':
             for i, row in enumerate(datosAnalisis.itertuples(),1):
                 peak_current += abs(row.peak_current)
 
+                # print(row.latitude,row.longitude)
+
                 # print(row.start_time, abs(row.peak_current))
                 if(peak_current >= 1000000):
                     print("########")
                     print("Posible evento severo. Amperaje de:", peak_current)
-
+                    plot.drawIntoMap(row.longitude, row.latitude, row.type)
                     # Convertir hora UTC a hora local UTC -3
                     horaEvento = row.start_time - timedelta(hours=3)
                     print(horaEvento)
@@ -48,7 +57,7 @@ if __name__ == '__main__':
 
         tiempoAnalizarIni = tiempoAnalizarFin
         tiempoAnalizarFin = tiempoAnalizarIni + timedelta(minutes=tiempoIntervalo)
-
+    plot.printMap()
     exit(0)
 
     """"
---
+++ util/PlotData.py
@@ -0,0 +1,42 @@
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+
+class Plot:
+
+    def __init__(self):
+        # Set the lower left and upper right limits of the bounding box:
+        lllon = -62.6509765625
+        urlon = -54.241796875
+        lllat = -27.5538085938
+        urlat = -19.2862304688
+
+        # and calculate a centerpoint, needed for the projection:
+        centerlon = float(lllon + urlon) / 2.0
+        centerlat = float(lllat + urlat) / 2.0
+
+        self.m = Basemap(resolution='i',  # crude, low, intermediate, high, full
+                    llcrnrlon=lllon, urcrnrlon=urlon,
+                    lon_0=centerlon,
+                    llcrnrlat=lllat, urcrnrlat=urlat,
+                    lat_0=centerlat,
+                    projection='tmerc')
+
+        # Read state boundaries.
+        shp_info =self.m.readshapefile('dataset/shp/PRY_adm1', 'states',
+                                   drawbounds=True, color='black')
+
+        # Read county boundaries
+        # shp_info = m.readshapefile('dataset/shp/PRY_adm2',
+        #                            'counties',
+        #                            drawbounds=True)
+
+    def drawIntoMap(self,x,y,type):
+        # -57.623154, -25.280073
+        x, y = self.m(x,y)
+        self.m.plot(x, y, 'bo', markersize=2)
+
+    def printMap(self):
+        plt.title('Paraguay')
+        # Get rid of some of the extraneous whitespace matplotlib loves to use.
+        # plt.tight_layout(pad=0, w_pad=0, h_pad=0)
+        plt.show()
