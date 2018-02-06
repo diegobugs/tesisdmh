f7a2323b0f3b6f6ca8780f540b521f50f76fa224
gabriel <gcgabcasco@gmail.com>
2018-02-05 22:23:51 -0300

    codigo para convertir imagenes en gif o mp4.

---
+++ gifymp4.py
@@ -0,0 +1,9 @@
+import glob
+import moviepy.editor as mpy
+
+video_name = './png/tormenta'  ##nombre del archivo
+fps = 1
+file_list = glob.glob ('./png/*.png')  # obtiene los png de la ruta actual
+# list.sort(file_list, key=lambda x: int(x.split('_')[1].split('.png')[0])) # Sort the images by #, this may need to be tweaked for your use case
+clip = mpy.ImageSequenceClip (file_list, fps=fps)
+clip.write_videofile ('{}.mp4'.format (video_name), fps=fps)
