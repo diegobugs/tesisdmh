357e25df3d0b918d010f8bf114ba349c8ee15e92
Diego <dieguitobugs@hotmail.com>
2018-02-17 18:12:56 -0300

    Recorrer precipitaciones, y descargas, preparando el modelo de ML

---
+++ ML_v1.py
@@ -0,0 +1,34 @@
+# Just disables the warning, doesn't enable AVX/FMA
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+
+# Model parameters
+W = tf.Variable([.3], tf.float32)
+b = tf.Variable([-.3], tf.float32)
+
+# Inputs and Outputs
+x = tf.placeholder(tf.float32)
+
+linear_model = W * x + b
+
+y = tf.placeholder(tf.float32)
+
+
+# Loss
+squared_delta = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_delta)
+
+# Optimizer
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+sess= tf.Session()
+sess.run(init)
+for i in range(1000):
+    sess.run(train, {x:[1,2,3,4],y:[0,-1,-2,-3]})
+print(sess.run([W,b]))
+# print(sess.run(loss, {x:[1,2,3,4],y:[0,-1,-2,-3]}))
