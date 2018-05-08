ade96231d22ab1ee071e950e79166664c142c706
Diego <dieguitobugs@hotmail.com>
2018-05-08 17:31:44 -0400

    v4

--- matrizConfusion_v2.py
+++ matrizConfusion_v2.py
@@ -4,26 +4,64 @@ print(__doc__)
 import itertools
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
+from sklearn.externals import joblib
 
 from sklearn import svm, datasets
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 
 # import some data to play with
-iris = datasets.load_iris()
-print(iris)
-exit(0)
-X = iris.data
-y = iris.target
-class_names = iris.target_names
+# y = [0, 5, 5, 10, 5, 0,10,10,10,5,5,0,10,10,10,0,5,10,10,0,5]
+# X = [[0, 0], [0, 5], [4, 5], [4, 10], [9, 10], [9,19],[3,11],[2,10],[1,10],[5,8],[3,8],[4,3],[3,13],[3,13],[1,12],[0,1],[5,9],[0,10],[4,16],[6,7],[5,5]]
+
+
+y = [0,0,0,0,0, 0, 0, 0, 0, 5, 5,5,5,5, 10,10,10,10,10,10,10,10,10,10,10,10]
+X = [[0,0],[0,1],[0,3], [9,19],[0,2], [4,3], [6,2], [0,4], [1,6], [6,12],[7,10],[8,13],[9,10], [4,10], [4,11],[4,12],[3,10],[2,10],[5,16],[4,20],[3,16],[4,10],[2,13],[4,18],[3,14],[4,17]]
+# X = []
+# y = []
+
+with open('dataset\clf_data.csv', 'rt') as clf_data:
+    spamreader = csv.reader(clf_data, delimiter=';')
+    for row in spamreader:
+        X.append([float(row[0]), float(row[1])])
+with open('dataset\clf_know.csv', 'rt') as clf_know:
+    spamreader = csv.reader(clf_know, delimiter=';')
+    for row in spamreader:
+        y.append(float(row[0]))
+
+
+
+X = np.array(X)
+y = np.array(y)
+
+# Ordenar X
+matrix = np.matrix(np.column_stack((X, y)))
+matrix = matrix[np.argsort(matrix.A[:, 1])]
+
+X = (np.delete(matrix, np.s_[2], axis=1)).tolist()
+y = ((np.delete(matrix, np.s_[0:2], axis=1)).ravel()).tolist()
+y = y[0]
+
+# print(X)
+# print(y)
+class_names = np.array(['Nada', 'Lluvia', 'Tormenta'])
+# iris = datasets.load_iris()
+# X = iris.data
+# y = iris.target
+# class_names = iris.target_names
 
 # Split the data into a training set and a test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=450)
+
+print(len(X_test))
+print(len(y_test))
 
 # Run classifier, using a model that is too regularized (C too low) to see
 # the impact on the results
-classifier = svm.SVC(kernel='linear', C=0.01)
-y_pred = classifier.fit(X_train, y_train).predict(X_test)
+# classifier = svm.SVC(kernel='linear', C=1, cache_size=8000, probability=True, class_weight='balanced')
+classifier = joblib.load('modelo.sav')
+y_pred = classifier.predict(X_test)
 
 
 def plot_confusion_matrix(cm, classes,
